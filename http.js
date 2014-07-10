0a87995c019e6acbee6237842e081b4204c23dcf
Ryan Copley <github@ryancopley.com>
2014-07-09 23:36:31 -0400

    Cleaned up

--- http.js
+++ http.js
@@ -1,55 +1,47 @@
-
 var express = require('express');
 var http = require('http');
 var path = require('path');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var app = express();
 var xmlParser = require('xml2json');
 var requestify = require('requestify');
 
-app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(express.static(path.join(__dirname + '.../public')));
 app.use(app.router);
 
 //Routes
-app.get("/:path", function (req, res){
-    console.log("Yolo");
-    console.log(req.params.path);
+app.get("/:path", function (req, res) {
+
     requestify
-    .request(req.params.path, {
-        method: 'GET',
-        timeout: 10000
-    })
-    .then(function (response) {
-        console.log("Response");
-        var rx = response.body;
+        .request(req.params.path, {
+            method: 'GET',
+            timeout: 10000
+        })
+        .then(function (response) {
+            var rx = response.body;
 
-        var options = {
-            object: true,
-            reversible: false,
-            coerce: true,
-            sanitize: true,
-            trim: true,
-            arrayNotation: false
-        };
+            var options = {
+                object: true,
+                reversible: false,
+                coerce: true,
+                sanitize: true,
+                trim: true,
+                arrayNotation: false
+            };
 
-        var json = xmlParser.toJson(rx, options);
-        res.json(json);
-    }).fail(function (err) {
-        console.log("err");
-        console.log(err);
-        res.json({
-            "error" : err,
-            "code" : "3",
+            var json = xmlParser.toJson(rx, options);
+            res.json(json);
+        }).fail(function (err) {
+            console.log("err");
+            console.log(err);
+            res.json({
+                "error": err,
+                "code": "3",
+            });
+
         });
-
-    });
 });
 
 //If the router can't find a route, it falls through to the error handler. 
@@ -64,8 +56,8 @@ app.use(function (req, res, next) {
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
     res.json({
-        "error" : "xml2js service failed",
-        "code" : "1"
+        "error": "xml2json service failed",
+        "code": "1"
     });
 });
 
